8fc8df660a7df323639a5d284a65355c52d8da19
Taras Barabash <tarasbarabash@gmail.com>
2018-09-14 19:48:10 +0300

    Fixed css

--- js/api.js
+++ js/api.js
@@ -33,15 +33,16 @@ Router.get("/recentRequests", (req, res) => {
         .toArray()
         .then(result => {
           Promise.map(result, obj => {
-            return request(
-              `http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${obj.user}&api_key=${
-                process.env.API_KEY
-              }&format=json`
-            ).then(response => {
-              response = JSON.parse(response);
-              obj["image"] = response.user.image[1]["#text"]
-              return obj;
-            });
+            // return request(
+            //   `http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${obj.user}&api_key=${
+            //     process.env.API_KEY
+            //   }&format=json`
+            // ).then(response => {
+            //   response = JSON.parse(response);
+            //   obj["image"] = response.user.image[1]["#text"]
+            //   return obj;
+            // });
+            return obj;
           }).then(results => {
             res.send(results);
           });
